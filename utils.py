ad54becaf63c304e71b45497966577b9d2f8a351
fededemo <fededemo@gmail.com>
2022-10-17 22:11:51 -0300

    agregar modelo Doble_LSTM_7epochs_40ml con accuracy 0.9993

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@
 import numpy as np
 import pandas as pd
 import warnings
+import datetime as dt
 
 # ML
 import scipy
@@ -200,7 +201,7 @@ def predict_test(model, data):
     pred = np.argmax(prob, axis=1).reshape(-1,1)    
     return pred
 
-def gen_csv_file(test_ids, pred, class_name):
+def gen_csv_file(test_ids, pred, class_name, name):
     output = np.stack((test_ids, pred), axis=-1)
     output = output.reshape([-1, 2])
 
@@ -208,10 +209,10 @@ def gen_csv_file(test_ids, pred, class_name):
     df.columns = ['id','expected']
     
     df['expected'] = df['expected'].map(pd.Series(oh_categories[class_name]))    
-    df.to_csv("kaggle_test_output.csv", index = False, index_label = False)
+    df.to_csv("outputs/kaggle_test_output_"+name+'_'+dt.datetime.today().strftime('%Y%m%d_%H%M%S')+".csv", index = False, index_label = False)
     return df
 
-def load_test_sequences_and_generate_prediction_file(model, test_data, max_len):
+def load_test_sequences_and_generate_prediction_file(model, test_data, max_len,name):
     raw_sequences_X_test = load_sequences(test_data)
     padded_sequences = pad_sequences(raw_sequences_X_test, max_len)
     
@@ -220,7 +221,7 @@ def load_test_sequences_and_generate_prediction_file(model, test_data, max_len):
     test_ids = test_data['id']
     test_ids = np.array(test_ids).reshape(-1,1)
 
-    return gen_csv_file(test_ids, pred, 'class')
+    return gen_csv_file(test_ids, pred, 'class',name)
 
 def class_weights(df, class_name) :
     # Hint: usar
